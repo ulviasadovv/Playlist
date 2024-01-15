582e2eeab931921b3acc668005f7c49d06748f92
ulviasadovv <ulvisha@code.edu.az>
2024-01-15 14:48:08 +0400

    fixed

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 
 //buttons
-const playButton = document.querySelector(".play");
-const prevButton = document.querySelector(".prev");
-const nextButton = document.querySelector(".next");
+const playButton = document.querySelector("#play");
+const prevButton = document.querySelector("#prev");
+const nextButton = document.querySelector("#next");
+
+let myRange = document.querySelector(".my-range");
+let song = document.querySelector(".audio");
+
+song.onloadedmetadata = function(){
+    myRange.max = song.durarion;
+    myRange.value = song.currentTime;
+}
 
 //tracks
 const track1 = document.querySelector("#first");
@@ -17,12 +25,49 @@ let updateTimer;
 //playlist
 const tracks = [track1, track2, track3];
 
+track1.style.display = "flex"
 track2.style.display = "none";
 track3.style.display = "none";
 
 
-document.addEventListener("click", funv());
+nextButton.onclick = function(){
+    
+    console.log("nextButton works");
+};
 
-function funv(){
-    alert("kkkk");
+prevButton.onclick = function(){
+    console.log("prevButton works")
 }
+
+
+console.log(playButton.classList)
+
+function playPause(){
+    if(playButton.classList.contains("fa-play")){
+        song.play();
+        playButton.classList.remove("fa-play");
+        playButton.classList.add("fa-pause");
+        console.log("play");
+    }else{
+        song.pause();
+        playButton.classList.remove("fa-pause");
+        playButton.classList.add("fa-play");
+        console.log("pause");
+    }
+}
+
+if(song.play()){
+    setInterval(() => {
+        myRange.value = song.currentTime;
+    }, 1000)
+}
+
+myRange.onchange = function () {
+    song.play();
+    song.currentTime = myRange.value;
+    playButton.classList.add("fa-pause");
+    playButton.classList.remove("fa-play");
+}
+
+console.log(playButton.classList)
+
